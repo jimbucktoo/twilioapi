633892586aa35699d5ebb87001629b61c5207b22
jimbucktoo <jamesliang.g@gmail.com>
2018-10-08 21:56:54 -0600

    initialization.

---
+++ README.md
@@ -0,0 +1,23 @@
+# twilioapi
+
+twilioapi is a website developed because I am too lazy to fill out all of the information to order dominos pizza so I created a web app that sends me an SMS text message via twilio to confirm my order and then it places my order via the dominos API.
+
+## Technologies
+
+
+* [Dominos API](https://www.npmjs.com/package/dominos) - This is a node.js API for integrating with the Domino's pizza APIs.
+* [Express](https://http://expressjs.com/) - Express.js, or simply Express, is a web application framework for Node.js.
+* [Google Firebase](https://firebase.google.com/) - Firebase is Google's mobile platform that helps you quickly develop high-quality apps and grow your business.
+* [Heroku](https://heroku.com/) - Heroku is a platform as a service (PaaS) that enables developers to build, run, and operate applications entirely in the cloud.
+* [jQuery](https://developers.google.com/speed/libraries/#jquery) - jQuery is a cross-platform JavaScript library designed to simplify the client-side scripting of HTML.
+* [Node](https://nodejs.org/en/) - Node.js is an open-source, cross-platform JavaScript run-time environment that executes JavaScript code outside of a browser.
+* [Twilio API](https://www.npmjs.com/package/twilio) - Cloud communications platform for building SMS, Voice & Messaging applications on an API built for global scale. 
+
+## Authors
+
+* **James Liang** - *Initial work* - [jimbucktoo](https://github.com/jimbucktoo/jimbucktoo)
+
+## Pizza Order
+
+<img src="https://github.com/jimbucktoo/twilioapi/blob/master/pizzaGuy.PNG" width="500">
+<img src="https://github.com/jimbucktoo/twilioapi/blob/master/pizza.PNG" width="500">
---
+++ index.js
@@ -0,0 +1,187 @@
+require('dotenv/config');
+const accountSid = process.env.accountSID;
+const authToken = process.env.authTOKEN;
+
+const http = require('http');
+const express = require('express');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const pizzapi = require('dominos');
+
+const app = express();
+
+var firebase = require('firebase');
+require('firebase/auth');
+require('firebase/database');
+
+var firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAzngzitaO5nmWemiZoREbtEPyz5399ikw",
+    authDomain: "twilioapi-9d541.firebaseapp.com",
+    databaseURL: "https://twilioapi-9d541.firebaseio.com",
+    projectId: "twilioapi-9d541",
+    storageBucket: "twilioapi-9d541.appspot.com",
+    messagingSenderId: "586136443689"
+});
+
+var database = firebase.database();
+
+app.use(cors( {origin: true, credentials: true}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+function isYes(msg) {
+    return msg === 'yes' || msg === 'Yes' || msg === 'yes ' || msg === 'Yes ';
+}
+
+app.get('/', (req, res) => {
+    const client = require('twilio')(accountSid, authToken);
+
+    client.messages.create({
+        to: '+13149565183',
+        from: '13145825438',
+        body: 'Would you like to order a pizza? [ yes | no ]'
+    }).then(function(message) {
+        res.json(message);
+    }).catch(function(err){
+        res.send(err);
+    });
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    var firebaseObj = req.body;
+    function writeUserData() {
+        firebase.database().ref('/').set({
+            firebaseObj
+        });
+    }
+
+    writeUserData();
+    res.send(req.body)
+});
+
+app.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+    if(!isYes(req.body.Body)){
+        twiml.message('Your loss.');
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString());
+    } else {
+        pizzapi.Util.findNearbyStores(
+            '1644 Platte St., Denver, CO, 80203',
+            'Delivery',
+            function(storeData){
+                var readRef = firebase.database().ref('/firebaseObj');
+                readRef.on('value', function(snapshot) {
+                    var cardNumber = snapshot.val().cardnumber;
+                    var expMonth = snapshot.val().expmonth;
+                    var expYear = snapshot.val().expyear;
+                    var securityCode = snapshot.val().securitycode;
+                    var billingZip = snapshot.val().billingzip;
+                    var menuItem = snapshot.val().menuitem;
+                    var expDate = expMonth + expYear;
+
+                    var storeId = storeData.result.Stores[0].StoreID;
+                    var storeNumber = Number(storeId);
+
+                    var myStore = new pizzapi.Store(storeData.result.Stores[0]);
+                    myStore.ID = storeNumber;
+
+                    var firstName = 'James';
+                    var lastName = 'Liang';
+                    var email = 'jamesliang.g@gmail.com';
+                    var phoneNumber = '3149565183';
+
+                    var address = new pizzapi.Address({
+                        Street: '1644 Platte St.',
+                        City: 'Denver',
+                        Region: 'CO',
+                        PostalCode: '80202'
+                    });
+
+                    var customerProfile = new pizzapi.Customer(
+                        {
+                            firstName: firstName,
+                            lastName: lastName,
+                            address: address,
+                            email: email,
+                            phone: phoneNumber
+                        }
+                    );
+
+                    var order = new pizzapi.Order(
+                        {
+                            customer: customerProfile,
+                            storeID: myStore.ID,
+                            deliveryMethod: 'Delivery'
+                        }
+                    );
+
+                    order.addItem(
+                        new pizzapi.Item(
+                            {
+                                code: menuItem,
+                                options: {},
+                                quantity: 1
+                            }
+                        )
+                    );
+
+                    var cardNumber = cardNumber;
+
+                    var cardInfo = new order.PaymentObject();
+                    cardInfo.Amount = order.Amounts.Customer;
+                    cardInfo.Number = cardNumber;
+                    cardInfo.CardType = order.validateCC(cardNumber);
+                    cardInfo.Expiration = expDate;
+                    cardInfo.SecurityCode = securityCode;
+                    cardInfo.PostalCode = billingZip;
+                    order.Payments.push(cardInfo);
+
+                    order.validate(
+                        function(result) {
+                            console.log(result);
+                            console.log("Order validated.");
+
+                            order.price(
+                                function(result) {
+                                    console.log(result);
+                                    console.log("Order priced.");
+
+                                    order.place(
+                                        function(result) {
+                                            console.log('Order Placed Result: ' + JSON.stringify(result));
+                                            console.log("Order placed.");
+
+                                            pizzapi.Track.byPhone(
+                                                3149565183,
+                                                function(pizzaData){
+                                                    console.log('Pizza Data: ' + JSON.stringify(pizzaData));
+                                                }
+                                            );
+                                        }
+                                    );
+                                }
+                            );
+                        }
+                    );
+
+                    console.log('Order: ' + JSON.stringify(order));
+
+                    twiml.message('Order sent. Don\'t forget to tip the driver.');
+                    res.writeHead(200, {'Content-Type': 'text/xml'});
+                    res.end(twiml.toString());
+                });
+            }
+        );
+    }
+});
+
+const port = process.env.PORT || 1337;
+
+http.createServer(app).listen(port, () => {
+    console.log('port: ' + port);
+});
+
+console.log('200 OK');
